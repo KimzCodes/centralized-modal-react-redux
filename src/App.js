95366ebe84c5194f1a2d5e7f2674fc55c0eb835b
kareem nour <kimzcodes@gmail.com>
2021-12-21 20:44:48 +0200

    add content

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from './components/layout/Header';
+import Content from './components/layout/Content';
 import ModalContainer from './components/modal/ModalContainer';
 function App() {
   return (
     <div>
       <ModalContainer />
       <Header />
+      <Content />
     </div>
   );
 }
---
+++ src/components/layout/Content.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Content = () => {
+  return (
+    <div>
+      <div className='article'>
+        <h2>What is Lorem Ipsum? </h2>
+        <p>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley not only five
+          centuries, but also the leap into electronic typesetting....
+        </p>
+        <button>Read full article</button>
+      </div>
+    </div>
+  );
+};
+
+export default Content;
